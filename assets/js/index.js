ba61498478b2c10a38996fcf4fd1e5ae42f9c1df
tkml87 <>
2023-08-14 03:17:00 +0000

    add javascript codes

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+function ingredientsHover() {
+    document.getElementById('ingredients').firstElementChild.firstElementChild.style.fontSize = "150%";
+}
+function ingredientsNormal() {
+    document.getElementById('ingredients').firstElementChild.firstElementChild.style.fontSize = "100%";
+}
+
+function preparationHover() {
+    document.getElementById('preparation').firstElementChild.firstElementChild.style.fontSize = "150%";
+}
+function preparationNormal() {
+    document.getElementById('preparation').firstElementChild.firstElementChild.style.fontSize = "100%";
+}
